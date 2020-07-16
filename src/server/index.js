07c7c94c805ca08eb8d9b2fd2e7bae7d256981ce
pranith9810 <n.rao@mountblue.tech>
2020-07-16 16:38:02 +0530

    Number of matches a team won per year function has been added

--- src/server/index.js
+++ src/server/index.js
@@ -3,21 +3,31 @@ var d3 = require("d3");
 var iplFunctions = require("./ipl.js");
 
 let numberOfMatchesPerYear = {};
+let numberOfMatchesPerTeamPerYear = {};
 
 fs.readFile("../data/matches.csv","utf8",function(err,data){
-    if(err){
-        console.error("error has occured while reading the file")
-    }
-    else{
+    if(err){                                                            
+        console.error("error has occured while reading the file")       
+    }                                                                   
+    else{                                          
     dataOfMatches=d3.csvParse(data);     
     }
+    // console.log(dataOfMatches);
     numberOfMatchesPerYear = iplFunctions.matchesPerYear(dataOfMatches);
-fs.writeFile("../output/matchesPerYear.json", JSON.stringify(numberOfMatchesPerYear), 'utf8', function (err) {
+    fs.writeFile("../output/matchesPerYear.json", JSON.stringify(numberOfMatchesPerYear,null,4), 'utf8', function (err) {
         if (err) {
             console.error("An error occured while writing JSON Object to File.");
         }
         else
-        console.log("JSON file has been saved.");
+        console.log("Number of matches won per year JSON file has been saved.");
     }); 
+   numberOfMatchesPerTeamPerYear = iplFunctions.matchesWonPerTeamPerYear(dataOfMatches);
+   fs.writeFile("../output/noOfMatchesTeamWonPerYear.json", JSON.stringify(numberOfMatchesPerTeamPerYear,null,4), 'utf8', function (err) {
+    if (err) {
+        console.error("An error occured while writing JSON Object to File.");
+    }
+    else
+    console.log("Number of matches a team won per year JSON file has been saved.");
+}); 
 });
 
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,4 +1,4 @@
-module.exports = { matchesPerYear }
+module.exports = { matchesPerYear , matchesWonPerTeamPerYear }
 
 
 function matchesPerYear(data){
@@ -14,5 +14,30 @@ function matchesPerYear(data){
     return noOfMatchesPerYear;
     }
 
+    function matchesWonPerTeamPerYear(data){
+        let noOfMatchesPerTeam = {};
+        for(let index=0;index<data.length;index++){
+            let rowObj = data[index];
+            let seasonInObj = rowObj['season'];
+            let winnerInObj = rowObj['winner'];
+            let resultInObj = rowObj['result'];
+            if(resultInObj !== 'no result'){
+            if(seasonInObj in noOfMatchesPerTeam){
+                if(noOfMatchesPerTeam[seasonInObj][winnerInObj]){
+                    noOfMatchesPerTeam[seasonInObj][winnerInObj] ++;
+                }
+                else{
+                    noOfMatchesPerTeam[seasonInObj][winnerInObj]  = 1;
+                }
+                 
+            }
+            else{
+                  noOfMatchesPerTeam[seasonInObj] = {};
+                  noOfMatchesPerTeam[seasonInObj][winnerInObj]  = 1;
+            }
+        }
+     }
+      return noOfMatchesPerTeam;
+    }
 
 
