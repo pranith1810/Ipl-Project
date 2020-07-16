1d333812d5ced969f286eb32c3debda3d392e0cb
pranith9810 <n.rao@mountblue.tech>
2020-07-16 18:11:50 +0530

    Number of Extra runs per team in the year 2016 function has been added.

--- src/server/index.js
+++ src/server/index.js
@@ -4,15 +4,18 @@ var iplFunctions = require("./ipl.js");
 
 let numberOfMatchesPerYear = {};
 let numberOfMatchesPerTeamPerYear = {};
+let extraRunsPerTeam2016 = {};
+
+let dataOfMatches = []
+let dataOfDeliveries = []
 
 fs.readFile("../data/matches.csv","utf8",function(err,data){
     if(err){                                                            
-        console.error("error has occured while reading the file")       
+        console.error("error has occured while reading the matches file")       
     }                                                                   
     else{                                          
     dataOfMatches=d3.csvParse(data);     
     }
-    // console.log(dataOfMatches);
     numberOfMatchesPerYear = iplFunctions.matchesPerYear(dataOfMatches);
     fs.writeFile("../output/matchesPerYear.json", JSON.stringify(numberOfMatchesPerYear,null,4), 'utf8', function (err) {
         if (err) {
@@ -27,7 +30,26 @@ fs.readFile("../data/matches.csv","utf8",function(err,data){
         console.error("An error occured while writing JSON Object to File.");
     }
     else
-    console.log("Number of matches a team won per year JSON file has been saved.");
-}); 
+    console.log("Number of matches a team won per year JSON file has been saved."); 
+     }); 
 });
+fs.readFile("../data/deliveries.csv","utf8",function(err,data){
+    if(err){
+        console.error("error has occured while reading the deliveries file")
+    }
+    else{
+        dataOfDeliveries = d3.csvParse(data);  
+    }
+    //console.log(dataOfDeliveries);
+     
+    extraRunsPerTeam2016 = iplFunctions.extraRuns2016(dataOfMatches,dataOfDeliveries);
+    fs.writeFile("../output/noOfExtraRunsPerTeam2016.json", JSON.stringify(extraRunsPerTeam2016,null,4), 'utf8', function (err) {
+        if (err) {
+            console.error("An error occured while writing JSON Object to File.");
+        }
+        else
+        console.log("Number of extra runs per team in the year 2016 JSON file has been saved."); 
+         }); 
+  });
+
 
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,4 +1,4 @@
-module.exports = { matchesPerYear , matchesWonPerTeamPerYear }
+module.exports = { matchesPerYear , matchesWonPerTeamPerYear , extraRuns2016 }
 
 
 function matchesPerYear(data){
@@ -12,11 +12,11 @@ function matchesPerYear(data){
         }
     }
     return noOfMatchesPerYear;
-    }
+}
 
-    function matchesWonPerTeamPerYear(data){
-        let noOfMatchesPerTeam = {};
-        for(let index=0;index<data.length;index++){
+function matchesWonPerTeamPerYear(data){
+    let noOfMatchesPerTeam = {};
+    for(let index=0;index<data.length;index++){
             let rowObj = data[index];
             let seasonInObj = rowObj['season'];
             let winnerInObj = rowObj['winner'];
@@ -37,7 +37,37 @@ function matchesPerYear(data){
             }
         }
      }
-      return noOfMatchesPerTeam;
+      return noOfMatchesPerTeam;  
+}
+
+function extraRuns2016(matchesData,deliveriesData){
+   let extraRunsPerTeam2016 = {};
+   let startId2016 = null;
+   let endId2016 = null;
+   for(let index=0;index<matchesData.length;index++){
+       if(matchesData[index].season === '2016'){
+           if(!startId2016){
+               startId2016 = Number(matchesData[index].id);
+           }
+           endId2016 = matchesData[index].id;
+       }
+    
     }
+    endId2016 = Number(endId2016);
+    for(let index=0;index<deliveriesData.length;index++){
+       let rowObj = deliveriesData[index];
+       let bowlingTeamObj = deliveriesData[index]['bowling_team'];
+       let extraRunsObj = deliveriesData[index]['extra_runs'];
+       if(Number(rowObj['match_id']) >= startId2016 && Number(rowObj['match_id']) <= endId2016 ){
+          if(extraRunsPerTeam2016[bowlingTeamObj]){
+            extraRunsPerTeam2016[bowlingTeamObj] = Number(extraRunsPerTeam2016[bowlingTeamObj])+Number(extraRunsObj);
+          }
+          else{
+            extraRunsPerTeam2016[bowlingTeamObj] = Number(extraRunsObj);
+          }
+       }
+    }
+   return extraRunsPerTeam2016;
+}
 
 
