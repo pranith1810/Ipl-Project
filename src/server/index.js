6ac9b8a9d67f81fbb3c2527b387f67a3c349d538
pranith9810 <n.rao@mountblue.tech>
2020-08-11 13:31:11 +0530

    Added connection.end() at the end of file in index.js to end the database connection

--- src/server/index.js
+++ src/server/index.js
@@ -14,68 +14,69 @@ connection.connect((err) => {
     if (err) {
         console.error('Error while connecting to database' + err);
     }
-
     else {
         console.log('Connection to database successful!!');
-
-        iplFunctions.matchesPerYear(connection)
-            .then((result) => {
-                fs.writeFile("../output/matchesPerYear.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
-                    if (err) {
-                        throw err;
-                    }
-                    else {
-                        console.log("Number of matches played per year JSON file has been saved.");
-                    }
-                })
-            })
-            .catch(error => {
-                console.error('An error occurred while writing to numberOfMatchesPerYear.json file.' + error);
-            });
-
-        iplFunctions.matchesWonPerTeamPerYear(connection)
-            .then((result) => {
-                fs.writeFile("../output/noOfMatchesTeamWonPerYear.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
-                    if (err) {
-                        throw err;
-                    }
-                    else {
-                        console.log("Number of matches a team won per year JSON file has been saved.");
-                    }
-                });
-            })
-            .catch(error => {
-                console.error('An error occurred while writing to numberOfMatchesPerTeamPerYear.json file.' + error);
-            });
-
-        iplFunctions.extraRuns2016(connection)
-            .then((result) => {
-                fs.writeFile("../output/noOfExtraRunsPerTeam2016.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
-                    if (err) {
-                        throw err;
-                    }
-                    else {
-                        console.log("Number of extra runs per team in the year 2016  JSON file has been saved.");
-                    }
-                });
-            })
-            .catch(error => {
-                console.error('An error occurred while writing to noOfExtraRunsPerTeam2016.json file.' + error);
-            });
-
-        iplFunctions.economicalBowlers2015(connection)
-            .then((result) => {
-                fs.writeFile("../output/topEconomicalBowlers2015.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
-                    if (err) {
-                        throw err;
-                    }
-                    else {
-                        console.log("Top 10 economical bowlers in the year 2015 JSON file has been saved.");
-                    }
-                });
-            })
-            .catch(error => {
-                console.error('An error occurred while writing to topEconomicalBowlers2015.json file.' + error);
-            });
     }
 });
+
+iplFunctions.matchesPerYear(connection)
+    .then((result) => {
+        fs.writeFile("../output/matchesPerYear.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log("Number of matches played per year JSON file has been saved.");
+            }
+        })
+    })
+    .catch(error => {
+        console.error('An error occurred while writing to numberOfMatchesPerYear.json file.' + error);
+    });
+
+iplFunctions.matchesWonPerTeamPerYear(connection)
+    .then((result) => {
+        fs.writeFile("../output/noOfMatchesTeamWonPerYear.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log("Number of matches a team won per year JSON file has been saved.");
+            }
+        });
+    })
+    .catch(error => {
+        console.error('An error occurred while writing to numberOfMatchesPerTeamPerYear.json file.' + error);
+    });
+
+iplFunctions.extraRuns2016(connection)
+    .then((result) => {
+        fs.writeFile("../output/noOfExtraRunsPerTeam2016.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log("Number of extra runs per team in the year 2016  JSON file has been saved.");
+            }
+        });
+    })
+    .catch(error => {
+        console.error('An error occurred while writing to noOfExtraRunsPerTeam2016.json file.' + error);
+    });
+
+iplFunctions.economicalBowlers2015(connection)
+    .then((result) => {
+        fs.writeFile("../output/topEconomicalBowlers2015.json", JSON.stringify(result, null, 4), 'utf8', function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log("Top 10 economical bowlers in the year 2015 JSON file has been saved.");
+            }
+        });
+    })
+    .catch(error => {
+        console.error('An error occurred while writing to topEconomicalBowlers2015.json file.' + error);
+    });
+
+connection.end();
