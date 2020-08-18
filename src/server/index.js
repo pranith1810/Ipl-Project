50f47568a9100dca7792b8d577149932e2fcd820
pranith9810 <n.rao@mountblue.tech>
2020-08-18 17:27:24 +0530

    Connected to the database using createPool() instead of createConnection() in index.js file

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,13 @@
 let config = require("./config.js");
 let mysql = require('mysql');
 
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
     host: config.host,
     user: config.user,
     password: config.password,
     database: config.database
 });
 
-connection.connect((err) => {
-    if (err) {
-        throw new Error('Connection to database failed!!!'+err);
-    }
-    else {
-        console.log('Connection to database successful!!');
-    }
-});
-
-module.exports = {connection}
+module.exports = {connection};
 
 
